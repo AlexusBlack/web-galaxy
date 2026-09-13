ba4dd74893613d65b62f2b1924c6ef786cd547a9
Alex Chernov <alexusblack@gmail.com>
2026-09-13 13:42:16 +1000

    using star tiles texture, varying color and size

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import * as QUARKS from 'three.quarks';
+import { RandomColorIndependent } from './RandomColorIndependent.ts';
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x000000);
@@ -16,13 +17,17 @@ document.body.appendChild( renderer.domElement );
 const batchedRenderer = new QUARKS.BatchedRenderer();
 scene.add(batchedRenderer);
 
+const starTextureMap = new THREE.TextureLoader().load("images/SG_star_16pack.png");
 // Create a particle system
 const particleSystem = new QUARKS.ParticleSystem({
+  uTileCount: 4,
+  vTileCount: 4,
+  blendTiles: true,
   // Duration of the particle system in seconds
-  duration: 10,
+  duration: 30,
 
   // Whether the particle system should loop
-  looping: false,
+  looping: true, // FOR DEMO ONLY
 
   // Emission shape (where particles are emitted from)
   shape: new QUARKS.SphereEmitter({
@@ -44,11 +49,15 @@ const particleSystem = new QUARKS.ParticleSystem({
   // emissionOverTime: new QUARKS.ConstantValue(500),
 
   // Initial particle properties
-  startLife: new QUARKS.ConstantValue(30000000),
+  startLife: new QUARKS.ConstantValue(30),
   // prewarm: true,
   startSpeed: new QUARKS.ConstantValue(0),
-  startSize: new QUARKS.ConstantValue(30),
-  startColor: new QUARKS.ConstantColor(new THREE.Vector4(1, 1, 1, 1)),
+  startSize: new QUARKS.IntervalValue(22.5, 37.5),
+  // startColor: new QUARKS.ConstantColor(new THREE.Vector4(1, 1, 1, 1)),
+  startColor: new RandomColorIndependent(
+    new THREE.Vector4(0.65, 0.65, 0.65, 1),
+    new THREE.Vector4(1.00, 1.00, 1.00, 1),
+  ),
 
   // Whether to use world space coordinates
   worldSpace: true,
@@ -56,13 +65,19 @@ const particleSystem = new QUARKS.ParticleSystem({
   // Material for particles
   material: new THREE.MeshBasicMaterial({
     // color: 0xffffff,
-    map: new THREE.TextureLoader().load("images/SG_star.png"),
+    // map: new THREE.TextureLoader().load("images/SG_star.png"),
+    map: starTextureMap,
     transparent: true,
     blending: THREE.AdditiveBlending,
   }),
 
   // Behaviors controlling particle evolution over time
   behaviors: [
+    new QUARKS.FrameOverLife(new QUARKS.PiecewiseBezier([[new QUARKS.Bezier(0, 5, 10, 15), 0]])), // tile index 0 from the 4x4 grid
+    //new QUARKS.ColorOverLife(new QUARKS.RandomColor(
+    //  new THREE.Vector4(0.65, 0.65, 0.65, 1), // Start color (orange)
+    //  new THREE.Vector4(1.00, 1.00, 1.00, 0), // End color (dark gray, fully transparent)
+    //)),
     // new QUARKS.SizeOverLife(
     //   new QUARKS.PiecewiseBezier([[new QUARKS.Bezier(1, 0.8, 0.2, 0), 0]]),
     // ),
